b500fdcf69925f242402e6c1907a39add74cf6b6
Jakub Ścieszka <95029525+CyanurdePotassium@users.noreply.github.com>
2024-01-01 22:08:31 +0100

    Added variables importances visualization.

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,4 +1,30 @@
+# load the model
 from joblib import load
 
-# load the model
 model = load('../../models/RFC.joblib')
+
+# create importances dataframe and sort it
+import pandas as pd
+
+importances = pd.DataFrame(
+    dict(
+        names = model.feature_names_in_, 
+        weighs = model.feature_importances_.round(2)
+    )
+)
+
+importances = importances.sort_values('weighs')
+
+# create importances plot
+import matplotlib.pyplot as plt
+
+fig, bar = plt.subplots()
+
+fig.set_figheight(10)
+fig.set_figwidth(14)
+
+bar.barh(importances['names'], importances['weighs'], align = 'center', height = 1)
+bar.set_title("Variables importance")
+
+# save the plot
+fig.savefig("../../reports/figures/importances.png")
